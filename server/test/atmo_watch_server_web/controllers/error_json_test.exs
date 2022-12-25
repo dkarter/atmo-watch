faa16d6c2fca520008b4694658b719b6b6627b29
Dorian Karter <dkarter@users.noreply.github.com>
2022-12-25 16:40:23 -0600

    style: run formatter

--- server/test/atmo_watch_server_web/controllers/error_json_test.exs
+++ server/test/atmo_watch_server_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule AtmoWatchServerWeb.ErrorJSONTest do
   use AtmoWatchServerWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert AtmoWatchServerWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert AtmoWatchServerWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do

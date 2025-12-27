2c408c865060bd8e94739fd031bdc3196093c08e
Dorian Karter <dkarter@users.noreply.github.com>
2025-12-26 23:12:45 -0600

    fix(firmware): modernize to latest Nerves

--- firmware/mix.exs
+++ firmware/mix.exs
@@ -3,19 +3,22 @@ defmodule AtmoWatch.MixProject do
 
   @app :atmo_watch
   @version "0.1.0"
-  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4, :bbb, :osd32mp1, :x86_64, :grisp2]
+  @all_targets [:rpi0]
 
   def project do
     [
       app: @app,
       version: @version,
-      elixir: "~> 1.11",
-      archives: [nerves_bootstrap: "~> 1.11"],
+      elixir: "~> 1.17",
+      archives: [nerves_bootstrap: "~> 1.13"],
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      releases: [{@app, release()}],
-      preferred_cli_target: [run: :host, test: :host]
+      releases: [{@app, release()}]
     ]
+  end
+
+  def cli do
+    [preferred_targets: [run: :host, test: :host]]
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -29,38 +32,28 @@ defmodule AtmoWatch.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # Dependencies for all targets
-      {:nerves, "~> 1.7.16 or ~> 1.8.0 or ~> 1.9.0", runtime: false},
+      # Core Nerves Framework - Updated to latest
+      {:nerves, "~> 1.12.0", runtime: false},
       {:shoehorn, "~> 0.9.1"},
-      {:ring_logger, "~> 0.8.5"},
-      {:toolshed, "~> 0.2.26"},
-      {:circuits_gpio, "~> 1.0"},
-      {:circuits_spi, "~> 1.3"},
-      {:req, "~> 0.3"},
-      # {:vintage_net_wizard, "~> 0.4"},
-      {:plug, "~> 1.14"},
-      {:plug_cowboy, "~> 2.6"},
+      {:ring_logger, "~> 0.11"},
+      {:toolshed, "~> 0.4"},
+
+      # Hardware Libraries - Updated to v2.x
+      {:circuits_gpio, "~> 2.1"},
+      {:circuits_spi, "~> 2.0"},
       {:dht, "~> 0.1"},
 
-      # Dependencies for all targets except :host
-      {:nerves_runtime, "~> 0.13.0", targets: @all_targets},
-      {:nerves_pack, "~> 0.7.0", targets: @all_targets},
+      # HTTP/Networking - Updated to latest
+      {:req, "~> 0.5"},
+      {:plug, "~> 1.16"},
+      {:plug_cowboy, "~> 2.7"},
 
-      # Dependencies for specific targets
-      # NOTE: It's generally low risk and recommended to follow minor version
-      # bumps to Nerves systems. Since these include Linux kernel and Erlang
-      # version updates, please review their release notes in case
-      # changes to your application are needed.
-      {:nerves_system_rpi, "~> 1.19", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.19", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.19", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.19", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.19", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.19", runtime: false, targets: :rpi4},
-      {:nerves_system_bbb, "~> 2.14", runtime: false, targets: :bbb},
-      {:nerves_system_osd32mp1, "~> 0.10", runtime: false, targets: :osd32mp1},
-      {:nerves_system_x86_64, "~> 1.19", runtime: false, targets: :x86_64},
-      {:nerves_system_grisp2, "~> 0.3", runtime: false, targets: :grisp2}
+      # Runtime dependencies - Updated
+      {:nerves_runtime, "~> 0.13.8", targets: @all_targets},
+      {:nerves_pack, "~> 0.7.1", targets: @all_targets},
+
+      # System package for Raspberry Pi Zero
+      {:nerves_system_rpi0, "~> 1.29", runtime: false, targets: :rpi0}
     ]
   end
 
